bad1873f85a3ddf6e5541e97bdda60beeb7d05ab
ttek <dkakol01@gmail.com>
2019-04-10 13:52:25 +0200

    add prototype links to all league champions and connect to static data

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Hero = props => {
-  return <section>Hero name</section>;
+  return (
+    <section
+      style={{
+        backgroundImage: `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${
+          props.data.id
+        }_0.jpg)`,
+        height: "50vh"
+      }}
+    >
+      {props.data.name}
+    </section>
+  );
 };
 
 export default Hero;
--- src/layout/App.js
+++ src/layout/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import "../styles/App.css";
+import Page from "./Page";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <div className="App">work</div>
+        <div className="App">
+          work
+          <Page />
+        </div>
       </BrowserRouter>
     );
   }
