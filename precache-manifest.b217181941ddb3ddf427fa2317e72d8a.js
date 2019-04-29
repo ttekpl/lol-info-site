0831444c13b0226af7a46318c02cd11437a310f6
ttek <dkakol01@gmail.com>
2019-04-29 16:48:49 +0200

    Updates

---
+++ precache-manifest.b217181941ddb3ddf427fa2317e72d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dc7d2cc37f56e9b1fb8",
+    "url": "/lol-info-site/static/js/runtime~main.e13671b3.js"
+  },
+  {
+    "revision": "0d2b0205b473418ea3bf",
+    "url": "/lol-info-site/static/js/main.696101f8.chunk.js"
+  },
+  {
+    "revision": "1a915a78697cbd3adae7",
+    "url": "/lol-info-site/static/js/2.9f6e2d3a.chunk.js"
+  },
+  {
+    "revision": "0d2b0205b473418ea3bf",
+    "url": "/lol-info-site/static/css/main.ab8d385d.chunk.css"
+  },
+  {
+    "revision": "3ad78346dc30d8d9b0c973b097211d1d",
+    "url": "/lol-info-site/index.html"
+  }
+];
